dc2e0b8b1df49a08593c81056498c25a8fcdc2a7
d.emelyanov <d.emelyanov@betby.com>
2025-01-11 20:42:55 +0100

    update

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,9 @@ use rusqlite::{Connection, Result};
 use chrono::{DateTime, Utc};
 use std::error::Error;
 use std::fmt;
+use std::fs;
+use std::io::Write;
+use std::path::PathBuf;
 
 const SCHEMA_VERSION: i32 = 1;
 
@@ -44,39 +47,187 @@ impl fmt::Display for StorageError {
     }
 }
 
+impl fmt::Display for TimerStatus {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            TimerStatus::RUN => write!(f, "Running"),
+            TimerStatus::PAUSED => write!(f, "Paused"),
+            TimerStatus::CLOSED => write!(f, "Finished")
+        }
+    }
+}
+
 impl Error for StorageError {}
+
+
+impl Timer {
+    fn time_on(&self) -> i64 {
+        let time = match self.status {
+            TimerStatus::RUN => {
+                Utc::now().timestamp() - self.start.timestamp()
+            },
+            _ => {
+                self.end.timestamp() - self.start.timestamp()
+            }
+        };
+        time - self.idle
+    }
+    
+    fn print_time_on(time: i64) {
+        match time {
+            t if t <= 60 => println!("{} sec", t),
+            t if t <= 3600 => {
+                println!(
+                    "{} min {} sec",
+                    t / 60,
+                    t % 60
+                );
+            },
+            t if t <= 86400 => {
+                println!(
+                    "{} hours {} min {} sec",
+                    t / 3600,
+                    (t % 3600) / 60,
+                    t % 60
+                )
+            },
+            t if t <= 86400 => {
+                println!(
+                    "{} hours {} min {} sec",
+                    t / 3600,
+                    (t % 3600) / 60,
+                    t % 60
+                )
+            },
+            t if t <= 86400 => {
+                println!(
+                    "{} hours {} min {} sec",
+                    t / 3600,
+                    (t % 3600) / 60,
+                    t % 60
+                )
+            },
+            t => {
+                println!(
+                    "{} days {} hr {} min {} sec",
+                    t / 86400,
+                    (t % 86400) / 3600,
+                    (t % 3600) / 60,
+                    t % 60
+                );
+            }
+        }
+    }
+
+    pub fn print(&self) {
+        println!("\n=========================");
+        println!("Current task: {}", self.task);
+        println!("Started at: {}", self.start);
+        println!("Status: {}", self.status);
+        print!("Spent: ");
+        Timer::print_time_on(self.time_on());
+        println!("\n=========================");
+    }
+}
+
+pub struct TimerCollection {
+    items: Vec<Timer>
+}
+
+impl TimerCollection {
+    
+    pub fn new() -> TimerCollection {
+        TimerCollection { items: Vec::new() }
+    }
+
+    pub fn print_items(&self) {
+        self.items.iter().for_each(| t | t.print());
+    }
+
+    pub fn print_stat(&self) {
+        let mut n = 0;
+        let mut time_on = 0;
+        self.items.iter().for_each(| t | {
+            n += 1;
+            time_on += t.time_on()
+        });
+        println!("==>> TOTAL STAT <<==");
+        println!("N tasks: {n}");
+        print!("Total time: ");
+        Timer::print_time_on(time_on);
+
+        print!("Avg time: ");
+        Timer::print_time_on(time_on / n as i64);
+        
+    }
+
+    pub fn export(&self, path: &str) -> Result<(), Box<dyn Error>> {
+        let mut f = fs::File::create(path)?;
+        let mut n = 1;
+        writeln!(f, "n,start,end,status,time_on")?;
+        for t in self.items.iter() {
+            writeln!(
+                f,
+                "{},{},{},{},{}",
+                n,
+                t.start,
+                t.end,
+                t.status,
+                t.time_on()
+            )?;
+            n += 1;
+        };
+        Ok(())
+    }
+
+}
 
 
 #[derive(Debug)]
 pub struct Storage {
     conn: Connection,
+    path: PathBuf,
     current_timer: Option<Timer>
 }
 
 impl Storage {
 
-    fn get_version(conn: &Connection) -> i32 {
+    pub fn path(&self) -> &PathBuf {
+        &self.path
+    }
+
+    fn get_version(conn: &Connection) -> Option<i32> {
         match conn.query_row(
             "SELECT version FROM schema_version",
             [],
             | row | { Ok(row.get(0)?) }
         ) {
             Ok(ver) => ver,
+            Err(rusqlite::Error::QueryReturnedNoRows) => None,
             Err(e) => panic!("Database error: {e}")
         }
     }
 
-    pub fn new(path: &str) -> Result<Storage, Box<dyn Error>> {
+    pub fn new(path: PathBuf) -> Result<Storage, Box<dyn Error>> {
         let conn = Connection::open(&path)?;
 
         conn.execute("CREATE TABLE IF NOT EXISTS schema_version (
             version INTEGER
         )", [])?;
-        
-        conn.execute(
-            "INSERT INTO schema_version (version) VALUES (?1)",
-            rusqlite::params![SCHEMA_VERSION]
-        )?;
+       
+        match Storage::get_version(&conn) {
+            Some(v) => {
+                if v != SCHEMA_VERSION {
+                    return Err(Box::new(StorageError::SchemaVersionError));
+                }
+            },
+            None => {
+                conn.execute(
+                    "INSERT INTO schema_version (version) VALUES (?1)",
+                    rusqlite::params![SCHEMA_VERSION]
+                )?;
+            }
+        }
 
         conn.execute("CREATE TABLE IF NOT EXISTS timers (
             start INTEGER PRIMARY KEY,
@@ -86,25 +237,37 @@ impl Storage {
             status INTEGER
         )", [])?;
 
-        if Storage::get_version(&conn) != SCHEMA_VERSION {
-            return Err(Box::new(StorageError::SchemaVersionError));
-        }
 
         Ok(Storage {
-            conn, 
+            conn,
+            path,
             current_timer: None
         })
     }
 
-    pub fn build(path: &str) -> Result<Storage, Box<dyn Error>> {
+    pub fn build(path: PathBuf) -> Result<Storage, Box<dyn Error>> {
         let mut storage = Storage::new(path)?;
         storage.current_timer = storage.get_active_timer();
         Ok(storage)
     }
 
+    pub fn display(&self) {
+        match &self.current_timer {
+            Some(t) => t.print(),
+            None => println!("There is no active timer")
+        }
+    }
+
+    pub fn drop(&self) -> Result<(), Box<dyn Error>> {
+        if fs::exists(&self.path).unwrap_or(false) == true {
+            fs::remove_file(&self.path)?
+        }
+        Ok(())
+    }
+
     pub fn get_active_timer(&self) -> Option<Timer> {
         match self.conn.query_row(
-            "SELECT task, start, end, idle, status FROM timers WHERE status = ?1",
+            "SELECT task, start, end, idle, status FROM timers WHERE status != ?1",
             rusqlite::params![
                 TimerStatus::CLOSED as i32
             ],
@@ -131,7 +294,7 @@ impl Storage {
 
     pub fn start_timer(&mut self, task: &str) -> Result<(), Box<dyn Error>> {
         match &self.current_timer {
-            Some(_) => {
+            Some(_t) => {
                 Err(Box::new(StorageError::CurrentTimerNotClosed))
             },
             None => {
@@ -204,23 +367,76 @@ impl Storage {
                 Err(Box::new(StorageError::TimerDoesNotExists))
             },
             Some(timer) => {
-                let now = Utc::now();
+                let now = match timer.status {
+                    TimerStatus::RUN => Utc::now().timestamp(),
+                    _ => timer.end.timestamp()
+                };
                 self.conn.execute(
-                    "UPDATE timers SET end = ?1 WHERE start = ?",
+                    "UPDATE timers SET end=?1, status=?2 WHERE start=?3",
                     rusqlite::params![
-                        now.timestamp(),
+                        now,
+                        TimerStatus::CLOSED as i32,
                         timer.start.timestamp()
                     ]
                 )?;
-
                 self.current_timer = None;
                 Ok(())
             }
         }
     }
 
-
+    pub fn load_timers(
+        &self,
+        collection: &mut TimerCollection,
+        n: i32,
+        date_from: Option<String>,
+        date_to: Option<String>
+    ) -> Result<(), Box<dyn Error>> {
+        let date_from = date_from.map(|d| {
+            DateTime::parse_from_str(
+                format!("{} 00:00:00 +0000", d).as_str(),
+                "%Y-%m-%d %H:%M:%S %z"
+            ).unwrap().timestamp()
+        });
+        let date_to = date_to.map(|d| {
+            DateTime::parse_from_str(
+                format!("{} 00:00:00 +0000", d).as_str(),
+                "%Y-%m-%d %H:%M:%S %z"
+            ).unwrap().timestamp()
+        });
+        let query = "
+            SELECT task, start, end, idle, status
+            FROM timers
+            WHERE
+                (?1 is NULL OR start >= ?1)
+                AND (?2 is NULL OR start <= ?2)
+            ORDER BY start DESC
+            LIMIT COALESCE(?3, -1)
+        ";
+        let mut stmt = self.conn.prepare(query)?;
+        let items = stmt.query_map(
+            rusqlite::params![date_from, date_to, n],
+            | row | {
+                Ok(Timer {
+                    task: row.get(0)?,
+                    start: DateTime::from_timestamp(row.get(1)?, 0).unwrap(),
+                    end: DateTime::from_timestamp(row.get(2)?, 0).unwrap(),
+                    idle: row.get(3)?,
+                    status: match row.get(4)?{
+                        1 => TimerStatus::RUN,
+                        2 => TimerStatus::PAUSED,
+                        3 => TimerStatus::CLOSED,
+                        _ => panic!("not supported value")
+                    },
+                })
+            }
+        )?;
+        collection.items.clear();
+        collection.items.extend(items.filter_map(Result::ok));
+        Ok(())
+    }
 }
+
 
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 use clap::{Parser, Subcommand};
-use focus_timer::{Timer, Storage};
-use std::error::Error;
+use focus_timer::{Storage, TimerCollection};
+use std::path::PathBuf;
+use dirs;
 
-const DB_PATH: &str = "test.db";
+
+fn get_default_db_path() -> PathBuf {
+    if let Some(data_dir) = dirs::data_local_dir() {
+        let app_dir = data_dir.join("focus_timer");
+        return app_dir.join("database.db");
+    }
+    PathBuf::from("database.db")
+}
 
 
 #[derive(Subcommand)]
@@ -11,22 +19,43 @@ enum Commands {
         #[arg(short, long)]
         task: String
     },
+    Info,
     Pause,
     Stop,
+    Drop,
     Export {
-        #[arg(short, long)]
-        date_from: String,
+        #[arg(long, short)]
+        path: String,
 
-        #[arg(short, long)]
-        date_to: String
+        #[arg(long)]
+        date_from: Option<String>,
+
+        #[arg(long)]
+        date_to: Option<String>,
+
+        #[arg(long, short)]
+        n: Option<i32>
+    },
+    List {
+        #[arg(long)]
+        date_from: Option<String>,
+
+        #[arg(long)]
+        date_to: Option<String>,
+
+        #[arg(long, short)]
+        n: Option<i32>
     },
     Stat {
-        #[arg(short, long)]
-        date_from: String,
+        #[arg(long)]
+        date_from: Option<String>,
 
-        #[arg(short, long)]
-        date_to: String
-    }
+        #[arg(long)]
+        date_to: Option<String>,
+
+        #[arg(long, short)]
+        n: Option<i32>
+    },
 }
 
 #[derive(Parser)]
@@ -35,20 +64,79 @@ struct Cli {
     command: Option<Commands>
 }
 
-fn start_timer(task: &str, storage: &Storage) {
-    let timer = Timer::new(task);
-    let _ = storage.add_timer(&timer).expect("Timer was not added");
-}
 
 fn main() {
-    let storage = Storage::new(DB_PATH).expect("DB not created");
+    let db_path = std::env::var("APP_DB_PATH")
+        .map(PathBuf::from)
+        .unwrap_or_else(|_| get_default_db_path());
+    let mut storage = Storage::build(db_path.into()).expect("DB not created");
+    let mut collection = TimerCollection::new();
     let cli = Cli::parse();
     match &cli.command {
-        Some(Commands::Start { task }) => start_timer(task, &storage),
-        Some(Commands::Pause) => todo!(),
-        Some(Commands::Stop) => todo!(),
-        Some(Commands::Export { date_from, date_to }) => todo!(),
-        Some(Commands::Stat { date_from, date_to }) => todo!(),
-        None => todo!()
+        Some(Commands::Info) => {
+            println!("Using database at: {}", storage.path().display())
+        },
+        Some(Commands::Start { task }) => {
+            match storage.start_timer(task) {
+                Ok(()) => println!("Task started"),
+                Err(e) => panic!("{e}")
+            };
+        },
+        Some(Commands::Drop) => {
+            storage.drop().expect("Can't remove");
+        }
+        Some(Commands::Pause) => {
+            match storage.pause_timer() {
+                Ok(()) => println!("Task is paused"),
+                Err(e) => panic!("{e}")
+            };
+        },
+        Some(Commands::Stop) => {
+            match storage.stop_timer() {
+                Ok(()) => println!("Task is stopped"),
+                Err(e) => panic!("{e}")
+            };
+        },
+        Some(Commands::Export { path, date_from, date_to, n }) => {
+            match storage.load_timers(
+                &mut collection,
+                n.unwrap_or(-1),
+                date_from.clone(),
+                date_to.clone()
+            ) {
+                Ok(()) => {},
+                Err(e) => panic!("{e}")
+            };
+            match collection.export(path) {
+                Ok(()) => println!("Saved"),
+                Err(e) => panic!("{e}")
+            }
+        },
+        Some(Commands::Stat { date_from, date_to, n }) => {
+            match storage.load_timers(
+                &mut collection,
+                n.unwrap_or(-1),
+                date_from.clone(),
+                date_to.clone()
+            ) {
+                Ok(()) => {},
+                Err(e) => panic!("{e}")
+            };
+            collection.print_stat();
+        },
+        Some(Commands::List { date_from, date_to, n }) => {
+            match storage.load_timers(
+                &mut collection,
+                n.unwrap_or(-1),
+                date_from.clone(),
+                date_to.clone()
+            ) {
+                Ok(()) => {},
+                Err(e) => panic!("{e}")
+            };
+            collection.print_stat();
+            collection.print_items();
+        }
+        None => storage.display()
     }
 }
